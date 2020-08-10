cfe1a738c5bcf9f4075109d8f9c4bbf0b6a675bf
pratikKataria <pratikkatariya3.pk@gmail.com>
2020-08-11 01:05:42 +0530

    app.js & index.html - client name selector added

--- public/app.js
+++ public/app.js
@@ -5,7 +5,11 @@ db.collection("clients").doc("client-names").get().then((doc) => {
     var namesObject = JSON.stringify(doc.data());
     var names = JSON.parse(namesObject)['names'];
      
+    var selectorMenu = document.getElementById("clientNamesSelector");
     for(var i =0; i < names.length; i++) {
+        var option = document.createElement("option");
+        option.text = names[i];
+        selectorMenu.add(option);
         console.log(names[i]);
     }
 
