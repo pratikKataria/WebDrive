0962eb4e9b6346a33564d1119f31e12e00aff68c
pratikKataria <pratikkatariya3.pk@gmail.com>
2020-08-10 11:58:14 +0530

    main.html - basic radio button added

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-alert("Hello! I am an alert box!!");
+var db = firebase.firestore();
+
+function getClientList() {
+    console.log("initn");
+    db.collection("user").doc("user-name").get().then((snapshot) => {
+        console.log(snapshot);
+    })
+
+}
