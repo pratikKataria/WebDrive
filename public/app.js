a0ea7d0e0f3d39077428373dad49dfe27dc9e9ab
pratikKataria <pratikkatariya3.pk@gmail.com>
2020-08-11 10:19:43 +0530

    app.js - Firebase Storage file upload completed

--- public/app.js
+++ public/app.js
@@ -14,3 +14,44 @@ db.collection("clients").doc("client-names").get().then((doc) => {
     }
 
 });
+
+
+
+uploadFile();
+populateYear();
+
+
+
+
+function uploadFile() {
+
+    var fileButton = document.getElementById("selectFile");
+
+    fileButton.addEventListener('change', function(e) {
+
+        var file = e.target.files[0];
+        var storageRef = firebase.storage();
+
+        var storage = storageRef.ref('demo/' + file.name);
+
+        storage.put(file);
+
+        console.log('file uploaded ');
+    });
+}
+
+
+
+
+
+
+// function for selection of year
+function populateYear() {
+    var start = new Date().getFullYear();
+    var end = 1900;
+    var option ="";
+    for (year = start; year >= end; year--) {
+        option += "<option>" + year +"</option>"
+    }
+    document.getElementById("year").innerHTML = option;
+}
